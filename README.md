cff4d2c5e7cf71fd8f6740119b57ff2327c77b58
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-04-07 01:28:58 -0500

    Check by modification time && set time interval

--- README.md
+++ README.md
@@ -1,30 +1,29 @@
-# fw
 
-```
-fw
+	fw 0.2.0
+	file-watcher
+	
+	USAGE:
+	    fw [OPTIONS] <PATH> <COMMAND>
+	
+	ARGS:
+	    <PATH>       File to watch
+	    <COMMAND>    Command to execute
+	
+	OPTIONS:
+	    -h, --help           Print help information
+	    -t, --time <TIME>    Check interval time [default: 2.5]
+	    -V, --version        Print version information
 
-USAGE:
-    fw <PATH> <COMMAND>
-
-ARGS:
-    <PATH>       File to watch
-    <COMMAND>    Command to execute
-
-OPTIONS:
-    -h, --help    Print help information
-```
-
-### Example 
+</br>
 
 Want to run a *justfile* script that compiles a `.tex` file each time it is saved.
 
-    fw main.tex just
-    
+	fw main.tex just
+
 Hence, `just` is executed each time main.tex changes.
 
+</br>
 
 ## Installation
 
-```
-cargo install --git https://github.com/AOx0/fw
-```
+	cargo install --git https://github.com/AOx0/fw
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use clap::Parser;
-use std::borrow::{Borrow, BorrowMut};
-use std::fs::OpenOptions;
+use std::borrow::BorrowMut;
+use std::fs::{File, OpenOptions};
 use std::io::Read;
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread::sleep;
 use std::time::Duration;
 use subprocess::NullFile;
 
 #[derive(Parser)]
+#[clap(version, about = "file-watcher")]
 struct Args {
     /// File to watch
     pub path: PathBuf,
 
     /// Command to execute
     pub command: String,
+
+    /// Check interval time
+    #[clap(short, long, default_value = "2.5")]
+    pub time: f32,
+
+    /// Whether a deep check must be made
+    #[clap(short, long)]
+    pub deep: bool,
 }
 
 macro_rules! printf {
@@ -32,44 +41,92 @@ fn main() {
     let args = Args::parse();
     let contents: Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(Vec::with_capacity(5000)));
     let mut sum: (u128, u128) = (0, 0);
+    let mut lens: (usize, usize) = (0, 0);
+    let mut last_modified: std::time::SystemTime = std::time::SystemTime::now();
 
     if args.path.exists() {
+        let mut first_time = true;
         loop {
             let file = OpenOptions::new().read(true).open(&args.path);
 
             if let Ok(mut f) = file {
-                if f.read_to_end(contents.lock().unwrap().borrow_mut()).is_ok() {
-                    for &b in contents.lock().unwrap().borrow().iter() {
-                        sum.0 += b as u128;
-                    }
-                } else {
-                    panic!("Error. Something happened while reading contents!");
+                let modified = f.metadata().unwrap().modified().unwrap();
+
+                if modified != last_modified {
+                    execute_command(&args);
                 }
 
-                if sum.0 != sum.1 {
-                    printf!("File changed. Executing \"{}\"... ", &args.command);
-                    let status = subprocess::Exec::shell(&args.command)
-                        .stdin(NullFile)
-                        .stdout(NullFile)
-                        .stderr(NullFile)
-                        .join()
-                        .unwrap();
+                last_modified = modified;
 
-                    if !status.success() {
-                        panic!("Something went wrong");
-                    }
-                    printf!("Ended\n");
+                if args.deep {
+                    deep_check(
+                        &args,
+                        contents.lock().unwrap(),
+                        &mut sum,
+                        &mut lens,
+                        &mut first_time,
+                        &mut f,
+                    );
                 }
-
-                sum = (0, sum.0);
-                contents.lock().unwrap().borrow_mut().clear();
             } else {
                 file.unwrap();
             }
 
-            sleep(Duration::from_secs_f32(2.5));
+            if first_time {
+                first_time = false;
+            }
+            sleep(Duration::from_secs_f32(args.time));
         }
     } else {
         panic!("Error. Path prob does not exist!");
     }
+}
+
+fn deep_check(
+    args: &Args,
+    mut contents: MutexGuard<Vec<u8>>,
+    sum: &mut (u128, u128),
+    lens: &mut (usize, usize),
+    first_time: &mut bool,
+    f: &mut File,
+) {
+    if f.read_to_end(contents.borrow_mut()).is_ok() {
+        {
+            let c = &contents;
+            lens.0 = c.len();
+
+            if lens.0 != lens.1 && !*first_time {
+                execute_command(args);
+            } else {
+                c.iter().for_each(|&n| sum.0 += n as u128);
+
+                if sum.0 != sum.1 {
+                    execute_command(args);
+                }
+            }
+
+            *lens = (0, lens.0);
+            *sum = (0, sum.0);
+        }
+
+        contents.borrow_mut().clear();
+    } else {
+        panic!("Error. Something happened while reading contents!");
+    }
+}
+
+fn execute_command(args: &Args) {
+    printf!("File changed. Executing \"{}\"...", &args.command);
+    let status = subprocess::Exec::shell(&args.command)
+        .stdin(NullFile)
+        .stdout(NullFile)
+        .stderr(NullFile)
+        .join()
+        .unwrap();
+
+    if !status.success() {
+        panic!("Something went wrong");
+    }
+
+    printf!("Success!\n");
 }
